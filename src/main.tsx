8c5b36e5052792ed085323cccbdeae159f32b457
denis-mahei <den.waves@gmail.com>
2025-09-19 14:25:39 +0300

    initial commit: noteHub app

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+import App from './components/App/App.tsx'
+import './index.css'
+// @ts-ignore
+import 'modern-normalize'
+
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+      <QueryClientProvider client={queryClient}>
+    <App />
+
+      </QueryClientProvider>
+  </StrictMode>,
+)
